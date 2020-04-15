ce6c8131a26ef6cc5386f8c4dd6380d7469ebc28
Maxwell Baird <bairdm71@gmail.com>
2020-04-15 10:48:05 -0600

    removed unnessary method

--- palindrome.rb
+++ palindrome.rb
@@ -1,19 +1,11 @@
 # write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
-def check_reverse(number)
-  if number == reverse(number)
-    true
-  else
-    false
-  end
-end
-
 def reverse(number)
   number.to_s.reverse.to_i
 end
 
 def palindrome(number)
   sum = number + reverse(number)
-  check_reverse(sum)
+  sum == reverse(sum)
 end
 
 holder = []
