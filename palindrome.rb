be9489b3322265b6affb0ca73dd74c7fc91cd3ea
Kevin McGrevey <niv3kmcg@gmail.com>
2020-04-15 10:39:14 -0600

    Pseudo code logic

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,33 @@
 # write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+require 'pry'
+
+number = 10
+
+# until number == 10 do
+1.times do
+  # string = number.to_s
+  number_to_string = number.to_s
+  string_reverse = number.to_s.reverse
+  binding.pry
+end
+
+
+
+
+# range 1..1000
+
+# a = 50 break into array 5,0
+# b = reverse array 0,5
+
+# total = a.into_integer(ai) + b.into_integer(bi)
+
+# total_to_array ai, bi
+
+# check  ai == bi
+
+# if yes
+#   convert back to integer
+#   pali << converted
+# else
+#   next number
+# end
