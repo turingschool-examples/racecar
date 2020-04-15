24c44378d9ee6b2d69b46d69e1fb2929622bfa19
Javier Aguilar <jvr.agx@gmail.com>
2020-04-15 10:42:57 -0600

    Rename sum_of_num to reverse_and_add

--- palindrome.rb
+++ palindrome.rb
@@ -3,12 +3,12 @@
 
 range = (1..1000).to_a
 
-def sum_of_num(num)
+def reverse_and_add(num)
   num.to_s.to_i + num.to_s.reverse.to_i
 end
 
 def numeric_palindrome?(num)
-  sum_of_num(num) == sum_of_num(num).to_s.reverse.to_i
+  reverse_and_add(num) == reverse_and_add(num).to_s.reverse.to_i
 end
 
 def find_numeric_palindromes(range)
