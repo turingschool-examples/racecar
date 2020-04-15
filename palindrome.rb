59186a18a255f6e035ff97a8f5a022a71514fd12
Brian Greeson <3604068+brian-greeson@users.noreply.github.com>
2020-04-15 10:56:51 -0600

    Finished with only one string conversion

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,19 @@
-# write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+require 'pry'
+
+def plalindrome?(text)
+  text == text.reverse
+end
+
+def reverse_i(number)
+  reverse = 0
+  while number > 0
+    reverse = (reverse * 10) + (number % 10)
+    number = number / 10
+  end
+  reverse
+end
+
+p (1..1000).select {|num| plalindrome?((num + reverse_i(num)).to_s)}
+
+
+
