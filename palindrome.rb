9316d3c9b657a0d66fd6b28ce37fc2a75e9d0768
Ethan Hocking <eshocking253@gmail.com>
2020-04-15 10:58:47 -0600

    Increase sample size, add note on times

--- palindrome.rb
+++ palindrome.rb
@@ -45,7 +45,7 @@ puts "\n"
 
 
 # the race is on!
-times1 = (0..100_000).reduce([]) do |times|
+times1 = (0..10_000_000).reduce([]) do |times|
   starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
   Palindromes.is_palindrome_sans_recursion?("racecar")
   ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
@@ -54,7 +54,7 @@ times1 = (0..100_000).reduce([]) do |times|
 end
 puts "average time for mine: #{avg1 = times1.sum(0.0) / times1.length}"
 
-times2 = (0..100_000).reduce([]) do |times|
+times2 = (0..10_000_000).reduce([]) do |times|
   starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
   Palindromes.is_palindrome_sans_recursion?("racecar")
   ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
@@ -62,6 +62,11 @@ times2 = (0..100_000).reduce([]) do |times|
   times << elapsed
 end
 puts "average time for sans recursion: #{avg2 = times2.sum(0.0) / times2.length}"
+
+# NOTE: the times for both the recursive and the .reverse methods
+# are compareable. From my testing, it seems like the .reverse method
+# has shorter minimum times, but occasionally spikes into running very slow
+# (or relatively slower, we're talking fractions of a fraction of a millisecond)
 
 
 if avg1 < avg2
