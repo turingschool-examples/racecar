b9fd2512a2db9230b5d883d628a3bd1d23445a2c
Mariana Cid <cidmariana5@gmail.com>
2020-04-15 11:15:03 -0600

    Palindromes?

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,12 @@
-# write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+# write a script in this file that finds all numbers between 1 and 1000 
+# that when reversed, and added to itself, create a numeric palindrome.
+
+array = (1..1000).to_a
+palindromes = []
+array.each do |number|
+  if number.to_s == number.to_s.reverse
+    palindromes << number.to_i
+  end
+end
+
+p palindromes
