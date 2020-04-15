9b5153c75745be78100301dbbe1c4ccd0dfb603c
Maxwell Baird <bairdm71@gmail.com>
2020-04-15 10:16:37 -0600

    create the method palindrome which adds a number and the reverse version together and checks if the sum is a palindrome

--- palindrome.rb
+++ palindrome.rb
@@ -1,14 +1,27 @@
 # write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
 def check_reverse(number)
-  if number == number.to_s.reverse.to_i
+  if number == reverse(number)
     true
   else
     false
   end
 end
 
-puts check_reverse(55)
-puts check_reverse(121)
-puts check_reverse(515)
-puts check_reverse(12)
-puts check_reverse(36)
+def reverse(number)
+  number.to_s.reverse.to_i
+end
+
+def palindrome(number)
+  sum = number + reverse(number)
+  check_reverse(sum)
+end
+
+holder = []
+
+1.upto(1000) do |number|
+  if palindrome(number)
+    holder << number
+  end
+end
+
+puts holder
