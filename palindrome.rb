b8cc9404e811be1d9e58fad611e9d23a5284f58e
Steven Meyers <sjjmeyers@gmail.com>
2020-04-15 10:57:31 -0600

    Add palindrome finder script

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,18 @@
-# write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+def find_palindromes
+  range = (1..1000).to_a
+  range.map do |value|
+    if reverse_and_add(value) == reverse_and_add(value).reverse
+      value
+    end
+  end.compact
+end
+
+def reverse_and_add(value)
+  (value + reverse_integer(value)).to_s
+end
+
+def reverse_integer(value)
+  value.to_s.reverse.to_i
+end
+
+p find_palindromes
