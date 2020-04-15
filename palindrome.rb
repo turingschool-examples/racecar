73c5e22211381c89c6fd05116c2f7c477a274210
Jesse Gietzen <gietzenjesse@gmail.com>
2020-04-15 10:47:46 -0600

    add palindromable numbers logic from 1 to 1000

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,10 @@
 # write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+
+def palindromable(num)
+  a = num.digits.reverse!
+  b = num.digits
+  c = a.join.to_i + b.join.to_i
+  c.digits == c.digits.reverse
+end
+
+print (1..1000).to_a.find_all { |i| palindromable(i) }
