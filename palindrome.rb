373fe86725ef35fb167a522c155a6cd7ac558f08
Ethan Hocking <eshocking253@gmail.com>
2020-04-15 10:53:55 -0600

    Add is_palindrome_sans_recursion? method, and set preformance speed tests

--- palindrome.rb
+++ palindrome.rb
@@ -10,19 +10,11 @@ require "pry"
 class Palindromes
   def self.numeric_palindromes(range)
     range.reduce([]) do |palindromes, index|
-
-      original_string = index.to_s
-      reversed_original_string = original_string.reverse
-
-      reversed_number = reversed_original_string.to_i
-      big_number = index + reversed_number
-
-      big_number_string = big_number.to_s
+      big_number_string = (index + self.reverse_number(index)).to_s
 
       if self.is_palindrome?(big_number_string)
         palindromes << index
       end
-
       palindromes
     end
   end
@@ -34,10 +26,46 @@ class Palindromes
       string[0] == string[-1] && self.is_palindrome?(string.slice(1..-2))
     end
   end
+
+  def self.is_palindrome_sans_recursion?(string)
+    string == string.reverse
+  end
+
+  private
+
+  def self.reverse_number(number)
+    original_string = number.to_s
+    reversed_number = original_string.reverse.to_i
+  end
 end
 
 
 p Palindromes.numeric_palindromes(1..1000)
+puts "\n"
 
-p "racecar"
-p Palindromes.is_palindrome?("racecar")
+
+# the race is on!
+times1 = (0..100_000).reduce([]) do |times|
+  starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  Palindromes.is_palindrome_sans_recursion?("racecar")
+  ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  elapsed = ending - starting
+  times << elapsed
+end
+puts "average time for mine: #{avg1 = times1.sum(0.0) / times1.length}"
+
+times2 = (0..100_000).reduce([]) do |times|
+  starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  Palindromes.is_palindrome_sans_recursion?("racecar")
+  ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  elapsed = ending - starting
+  times << elapsed
+end
+puts "average time for sans recursion: #{avg2 = times2.sum(0.0) / times2.length}"
+
+
+if avg1 < avg2
+  puts "HA! mine's better"
+else
+  puts "Damn, alright you win :("
+end
