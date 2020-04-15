6d96daaa7b18fbc51b342462d393e53322d525e0
Javier Aguilar <jvr.agx@gmail.com>
2020-04-15 10:22:05 -0600

    Write script to find numeric palindroms between 1 and 1000

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,15 @@
-# write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+# write a script in this file that finds all numbers between 1 and 1000 that
+# when reversed, and added to itself, create a numeric palindrome.
+
+range = (1..1000).to_a
+palindromes = []
+
+def sum_of_num(num)
+  num.to_s.to_i + num.to_s.reverse.to_i
+end
+
+range.each do | num |
+  palindromes << num if sum_of_num(num) == sum_of_num(num).to_s.reverse.to_i
+end
+
+p palindromes
