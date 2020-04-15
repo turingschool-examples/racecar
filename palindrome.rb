aa6810f440e3d749bd29abe98ad5cd7c5ea19a5d
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-15 11:00:45 -0600

    Create palindrome class

--- palindrome.rb
+++ palindrome.rb
@@ -1,3 +1,5 @@
+#============================As Plain Ruby======================================
+
 one_to_a_thousand = (1..1000).to_a
 
 all_numbers_added = []
@@ -13,3 +15,33 @@ palindrome = all_numbers_added.find_all do |added_num|
 end
 
 p palindrome
+
+#=============================As a Class========================================
+
+class Palindrome
+  def initialize(number_range)
+    @palindrome_numbers = number_range.to_a
+  end
+
+  def summed_numbers
+    all_numbers_added = []
+
+    @palindrome_numbers.each do |base_num|
+      @palindrome_numbers.each do |num_to_be_added|
+        all_numbers_added << (base_num + num_to_be_added)
+      end
+    end
+    all_numbers_added
+  end
+
+  def find_palindromes
+    summed_numbers.find_all do |added_num|
+      added_num.to_s == added_num.to_s.reverse
+    end
+  end
+
+end
+
+one_to_a_thousand = Palindrome.new(1..1000)
+
+p one_to_a_thousand.find_palindromes
