80d02566d80bbccb2961607d12ea37658bee0429
Tyler Tomlinson <twtomlinson89@gmail.com>
2020-04-15 11:03:39 -0600

    WIP

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,27 @@
 # write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+
+require 'matrix'
+
+def numbers_array
+  (1..100).to_a
+end
+
+def morphed_nums_array
+  numbers_array.map { |num| num.digits.join.to_i }
+end
+
+def sums
+  nums = [numbers_array], [morphed_nums_array]
+  nums.transpose.map(&:sum)
+end
+
+def palindrome?(number)
+  number == reverse(number)
+end
+
+# arrays = [numbers_array], [morphed_nums_array]
+# arrays.map { |a| Vector[*a] }.inject(:+)
+
+# puts numbers_array
+# puts morphed_nums_array
+# puts final_move
