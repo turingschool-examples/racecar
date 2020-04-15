58d90c7225264ec83c59f109b80946818726431e
“Krista <stadler.krista@gmail..com>
2020-04-15 10:42:18 -0600

    Finished palindrome challenge

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,6 @@
 # write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+require './lib/number'
+
+numbers = Number.new
+ numbers.find_palindrome_values
+ p numbers.find_palindromes
