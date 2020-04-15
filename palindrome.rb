3da37fed0c8dc0b14dd2cc34e8cd72cd2e156fed
Maxwell Baird <bairdm71@gmail.com>
2020-04-15 10:08:39 -0600

    created a method that checks if a number is equal to its reverse value

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,14 @@
 # write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+def check_reverse(number)
+  if number == number.to_s.reverse.to_i
+    true
+  else
+    false
+  end
+end
+
+puts check_reverse(55)
+puts check_reverse(121)
+puts check_reverse(515)
+puts check_reverse(12)
+puts check_reverse(36)
