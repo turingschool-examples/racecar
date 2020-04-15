2aa3c04a274b3554492b9f93fd5b60d286d31632
Colin Alexander <colintalex@gmail.com>
2020-04-15 10:37:22 -0600

    Completes Palindrome script, prints answers to screen.

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,15 @@
 # write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+range = Range.new(1,1000).to_a
+palindromes = []
+
+range.each do |num|
+  inverted = num.to_s.reverse.to_i
+   sum = num + inverted
+   match = sum == sum.to_s.reverse.to_i
+
+ if match
+     palindromes << num
+   end
+end
+
+print palindromes
