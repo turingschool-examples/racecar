794c50d2b22c5e36ba7cd51d1edf7796f511c9da
Kelsha_Darby <ohanaipo@gmail.com>
2020-04-15 10:50:46 -0600

    Create palindrome code

--- palindrome.rb
+++ palindrome.rb
@@ -1,1 +1,15 @@
-# write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
+one_to_a_thousand = (1..1000).to_a
+
+all_numbers_added = []
+
+one_to_a_thousand.each do |base_num|
+  one_to_a_thousand.each do |num_to_be_added|
+    all_numbers_added << (base_num + num_to_be_added)
+  end
+end
+
+palindrome = all_numbers_added.find_all do |added_num|
+  added_num.to_s == added_num.to_s.reverse
+end
+
+p palindrome
