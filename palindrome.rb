42f8166a25e7e9409ad3ed3954e18fd1bbdc0f84
Tyler Porter <tylerpporter@gmail.com>
2020-04-15 10:40:31 -0600

    Refactor to create Palindrome class

--- palindrome.rb
+++ palindrome.rb
@@ -1,15 +1,25 @@
-# write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
-def reverse_and_add(num)
-  num + num.to_s.reverse.to_i
+class Palindrome
+
+  def palindrome_nums
+    range = Array (1..1000)
+    range.select {|num| palindrome?(num)}
+  end
+
+  private
+
+  def reverse_and_add(num)
+    num + num.to_s.reverse.to_i
+  end
+
+  def palindrome?(num)
+    sum = reverse_and_add(num)
+    forward = sum.to_s
+    backward = sum.to_s.reverse
+    forward == backward
+  end
+
 end
 
-def palindrome?(num)
-  sum = reverse_and_add(num)
-  forward = sum.to_s
-  backward = sum.to_s.reverse
-  forward == backward
-end
+palindromes = Palindrome.new
 
-range = Array (1..1000)
-
-puts range.select {|num| palindrome?(num)}
+puts palindromes.palindrome_nums
