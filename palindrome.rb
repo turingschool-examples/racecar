fb4030316d4ce69babef835fee7fddc46bfec684
Ethan Hocking <eshocking253@gmail.com>
2020-04-15 11:03:00 -0600

    Pull palindromes class out into own file

--- palindrome.rb
+++ palindrome.rb
@@ -1,43 +1,12 @@
 # write a script in this file that finds all numbers between 1 and 1000 that when reversed, and added to itself, create a numeric palindrome.
 require "pry"
+require_relative "lib/palindromes"
 
 # class NumericPalindromeFinder
 #
 # end
 #
 # NumericPalindromeFinder.new
-
-class Palindromes
-  def self.numeric_palindromes(range)
-    range.reduce([]) do |palindromes, index|
-      big_number_string = (index + self.reverse_number(index)).to_s
-
-      if self.is_palindrome?(big_number_string)
-        palindromes << index
-      end
-      palindromes
-    end
-  end
-
-  def self.is_palindrome?(string)
-    if string.length <= 1
-      return true
-    else
-      string[0] == string[-1] && self.is_palindrome?(string.slice(1..-2))
-    end
-  end
-
-  def self.is_palindrome_sans_recursion?(string)
-    string == string.reverse
-  end
-
-  private
-
-  def self.reverse_number(number)
-    original_string = number.to_s
-    reversed_number = original_string.reverse.to_i
-  end
-end
 
 
 p Palindromes.numeric_palindromes(1..1000)
